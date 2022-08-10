f8580624ea1fec6845922b3fc9bd4fe746a87f1c
Mudhdhoo <104865472+Mudhdhoo@users.noreply.github.com>
2022-08-10 16:27:58 +0200

    Update NN.py

--- NN.py
+++ NN.py
@@ -1,45 +1,37 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 # Model NN2 and NN2
-class NN1(nn.Module):
-    def __init__(self, x_size, z_size):
-        super(NN1, self).__init__()
-        self.dense1 = nn.Linear(x_size, 50)
-        self.dense2 = nn.Linear(50, 50)
-        self.dense3 = nn.Linear(50, 50)
-        self.dense4 = nn.Linear(50, z_size)
-        
+class NN(nn.Module):
+    def __init__(self, num_hidden, hidden_size, in_size, out_size, activation):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self.activation = activation
+        current_dim = in_size
+        for _ in range(num_hidden):
+            self.layers.append(nn.Linear(current_dim, hidden_size))
+            current_dim = hidden_size
+        self.layers.append(nn.Linear(current_dim, out_size))
+
     def forward(self, x):
-        x = F.relu(self.dense1(x))
-        x = F.relu(self.dense2(x))
-        x = F.relu(self.dense3(x))       
-        x = self.dense4(x)
-        
+        # Normalize input
+        for layer in self.layers[:-1]:
+            x = self.activation(layer(x))
+        x = self.layers[-1](x)
+
+        # Denormalize output
+
         return x
     
-class NN2(nn.Module):
-    def __init__(self, x_size, z_size):
-        super(NN2, self).__init__()
-        self.dense1 = nn.Linear(z_size, 50)
-        self.dense2 = nn.Linear(50, 50)
-        self.dense3 = nn.Linear(50, 50)
-        self.dense4 = nn.Linear(50, x_size)
-        
-    def forward(self, x):
-        x = F.relu(self.dense1(x))
-        x = F.relu(self.dense2(x))
-        x = F.relu(self.dense3(x))       
-        x = self.dense4(x)
-        
-        return x
-
-class Normalize(nn.Module):
-    def __init__(self, mean, std):
+# NN1 and NN2 combined
+class Main_Network(nn.Module):
+    def __init__(self, x_size, z_size, num_hidden, hidden_size, activation):
         super().__init__()
-        self.mean = mean
-        self.std = std
-
-    def forward(self, x):
-        return (x - self.mean) / self.std
+        self.net1 = NN(num_hidden, hidden_size, x_size, z_size, activation)
+        self.net2 = NN(num_hidden, hidden_size, z_size, x_size, activation)
+        
+    def forward(self, x, z):
+        output_xz = self.net1(x)    # Output from NN1
+        output_xzx = self.net2(output_xz)    # Output from NN2 with NN1 as input       
+        
+        return output_xz, output_xzx
