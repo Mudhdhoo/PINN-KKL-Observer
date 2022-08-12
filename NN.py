97845d056c89cffd43891f408faa0b0931111ab7
Mudhdhoo <104865472+Mudhdhoo@users.noreply.github.com>
2022-08-12 21:05:14 +0200

    Update NN.py

--- NN.py
+++ NN.py
@@ -3,35 +3,52 @@ from torch import nn
 
 # Model NN2 and NN2
 class NN(nn.Module):
-    def __init__(self, num_hidden, hidden_size, in_size, out_size, activation):
+    def __init__(self, num_hidden, hidden_size, in_size, out_size, activation, normalizer = None):
         super().__init__()
         self.layers = nn.ModuleList()
         self.activation = activation
+        self.normalizer = normalizer
         current_dim = in_size
         for _ in range(num_hidden):
             self.layers.append(nn.Linear(current_dim, hidden_size))
             current_dim = hidden_size
         self.layers.append(nn.Linear(current_dim, out_size))
 
-    def forward(self, x):
-        # Normalize input
+    def forward(self, tensor):
+        """
+        Forward method of the NN.
+        If a normalizer object is passed to the class, the network will normalize the input and denormalize the output.
+        """
+        # Normalize input here
+        if self.normalizer != None:
+            tensor = self.normalizer.Normalize(tensor).float()
+
         for layer in self.layers[:-1]:
-            x = self.activation(layer(x))
-        x = self.layers[-1](x)
+            tensor = self.activation(layer(tensor))
+        tensor = self.layers[-1](tensor)
 
-        # Denormalize output
-
-        return x
+        # Denormalize output here
+        if self.normalizer != None:
+            tensor = self.normalizer.Denormalize(tensor).float()
+        return tensor
     
 # NN1 and NN2 combined
 class Main_Network(nn.Module):
-    def __init__(self, x_size, z_size, num_hidden, hidden_size, activation):
+    def __init__(self, x_size, z_size, num_hidden, hidden_size, activation, normalizer = None):
         super().__init__()
-        self.net1 = NN(num_hidden, hidden_size, x_size, z_size, activation)
-        self.net2 = NN(num_hidden, hidden_size, z_size, x_size, activation)
+        self.normalizer = normalizer
+        self.net1 = NN(num_hidden, hidden_size, x_size, z_size, activation, normalizer)
+        self.net2 = NN(num_hidden, hidden_size, z_size, x_size, activation, normalizer)
         
     def forward(self, x, z):
         output_xz = self.net1(x)    # Output from NN1
         output_xzx = self.net2(output_xz)    # Output from NN2 with NN1 as input       
         
-        return output_xz, output_xzx
+        if self.normalizer != None:
+            norm_x_hat = self.normalizer.Normalize(output_xzx).float()
+            norm_z_hat = self.normalizer.Normalize(output_xz).float()
+        else:
+            norm_x_hat = output_xzx
+            norm_z_hat = output_xz
+
+        return output_xz, output_xzx, norm_z_hat, norm_x_hat
