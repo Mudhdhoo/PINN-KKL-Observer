e0eac9e1a65d5bdbf8d1f007d82fd38865069f36
Mudhdhoo <104865472+Mudhdhoo@users.noreply.github.com>
2022-08-17 16:36:51 +0200

    Update NN.py

--- NN.py
+++ NN.py
@@ -8,6 +8,7 @@ class NN(nn.Module):
         self.layers = nn.ModuleList()
         self.activation = activation
         self.normalizer = normalizer
+        self.mode = 'normal'
         current_dim = in_size
         for _ in range(num_hidden):
             self.layers.append(nn.Linear(current_dim, hidden_size))
@@ -21,7 +22,7 @@ class NN(nn.Module):
         """
         # Normalize input here
         if self.normalizer != None:
-            tensor = self.normalizer.Normalize(tensor).float()
+            tensor = self.normalizer.Normalize(tensor, self.mode).float()
 
         for layer in self.layers[:-1]:
             tensor = self.activation(layer(tensor))
@@ -29,7 +30,7 @@ class NN(nn.Module):
 
         # Denormalize output here
         if self.normalizer != None:
-            tensor = self.normalizer.Denormalize(tensor).float()
+            tensor = self.normalizer.Denormalize(tensor, self.mode).float()
         return tensor
     
 # NN1 and NN2 combined
@@ -39,14 +40,17 @@ class Main_Network(nn.Module):
         self.normalizer = normalizer
         self.net1 = NN(num_hidden, hidden_size, x_size, z_size, activation, normalizer)
         self.net2 = NN(num_hidden, hidden_size, z_size, x_size, activation, normalizer)
-        
-    def forward(self, x, z):
+        self.mode = 'normal'
+
+    def forward(self, x):
+        self.net1.mode = self.mode
+        self.net2.mode = self.mode
         output_xz = self.net1(x)    # Output from NN1
         output_xzx = self.net2(output_xz)    # Output from NN2 with NN1 as input       
-        
+
         if self.normalizer != None:
-            norm_x_hat = self.normalizer.Normalize(output_xzx).float()
-            norm_z_hat = self.normalizer.Normalize(output_xz).float()
+            norm_x_hat = self.normalizer.Normalize(output_xzx, self.mode).float()
+            norm_z_hat = self.normalizer.Normalize(output_xz, self.mode).float()
         else:
             norm_x_hat = output_xzx
             norm_z_hat = output_xz
