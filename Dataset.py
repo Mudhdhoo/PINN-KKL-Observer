0ed6ebbb7787a367b09d9ec0e95640fd83eb5d81
Mudhdhoo <104865472+Mudhdhoo@users.noreply.github.com>
2022-08-22 19:30:08 +0200

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -1,6 +1,7 @@
-from random import sample
 import torch
 import data_generation as data
+import numpy as np
+from Systems import System
 
 class DataSet(torch.utils.data.Dataset):
     """
@@ -8,42 +9,61 @@ class DataSet(torch.utils.data.Dataset):
     The set is split up into data for normal loss and data for physics loss.
 
     -------- Parameters --------
-    system: 
-        A system object created from classes within Systems.py
+    system: Systems
+        A system instance created from classes within Systems.py
 
-    M:
+    M: ndarray
         M matrix in the z dynamics.
 
-    K:
+    K: ndarray
         K matrix in the z dynamics.
 
-    a:
+    a: int
         Start time of ODE solver.
 
-    b:
+    b: int
         End time of ODE solver.
+    
+    N: int
+        Number of intervals in [a,b] with step size (b-a)/N
 
-    samples:
+    samples: int
         Numer of initial conditions to be sampled for data generation.
 
-    limits_normal:
+    limits_normal: ndarray
         Limits on the state sample space of initial conditions used to generate
         data to compute the normal loss.
 
-    sample_mode: int
-        Either 1 or 2. 
-        If 1, the samples for the physics datapoints will be generated from a separate set of
+    PINN_sample_mode: int
+        Either 'split set' or 'split traj. 
+        If 'split set', the samples for the physics datapoints will be generated from a separate set of
         initial conditions. To set a different limit on the state sample space for the physics
         points, use the set_physics_limit method. 
-        If 2, These points will be generated from the same initial conditions,
+        If 'split traj', These points will be generated from the same initial conditions,
         with every other sample in a given trajectory being assigned as a physics datapoint.
         Default set to 2.
+
+    data_gen_mode: str
+        Either 'negative forward' or 'backward sim'
+        If 'negative forward, start simulation from a given negative time untill 0. Use the outputs of
+        the simuluation to obtain z(0).
+        If 'backward sim', the system is simulated backwards from 0 to a given negative time. The outputs are
+        used to simulate z system forward to obtain z(0).
     """
 
-    def __init__(self, system, M, K, a, b, N, samples, limits_normal, sample_mode = 2):
+    def __init__(self, system: System, M: np.ndarray, K: np.ndarray, a: int, b: int, N: int , samples: int, limits_normal: np.ndarray, PINN_sample_mode: str = 'split traj', data_gen_mode: str = 'negative forward' ) -> None:
         super().__init__()
-        self.sample_mode = sample_mode
-        self.train_data = self.generate_data(system, M, K, a, b, N, samples, limits_normal, seed = 888)    # Generate synthetic data x, z, y
+        self.M = M
+        self.K = K
+        self.system = system
+        self.a = a
+        self.b = b
+        self.N = N
+        self.samples = samples
+        self.limits_normal = limits_normal
+        self.PINN_sample_mode = PINN_sample_mode
+        self.data_gen_mode = data_gen_mode
+        self.train_data = self.generate_data(seed = 888)    # Generate synthetic data x, z, y
         self.data_length = self.train_data[0].shape[0]*self.train_data[0].shape[1]         # Total number of samples
         x = torch.from_numpy(self.train_data[0]).view(self.data_length, system.x_size)  # Convert to tensors and reshape
         z = torch.from_numpy(self.train_data[1]).view(self.data_length, system.z_size)
@@ -53,7 +73,7 @@ class DataSet(torch.utils.data.Dataset):
         else:
             y = torch.from_numpy(self.train_data[2]).view(self.train_data[2].shape[0]*self.train_data[2].shape[1])
 
-        if sample_mode == 1:
+        if PINN_sample_mode == 'split set':
             # ----------------------- Normal loss data ----------------------- 
             self.x_data = x
             self.z_data = z
@@ -62,8 +82,7 @@ class DataSet(torch.utils.data.Dataset):
             #-----------------------------------------------------------------
 
             # ----------------------- Physics loss data ----------------------- 
-            self.limit_physics = limits_normal
-            self.train_data_ph = self.generate_data(system, M, K, a, b, N ,samples, self.limit_physics, seed = 8888)
+            self.train_data_ph = self.generate_data(seed = 8888)
             self.data_length_ph = self.train_data_ph[0].shape[0]*self.train_data_ph[0].shape[1]         # Total number of samples
             self.x_data_ph = torch.from_numpy(self.train_data_ph[0]).view(self.data_length_ph, system.x_size)
             self.z_data_ph = torch.from_numpy(self.train_data_ph[1]).view(self.data_length_ph, system.z_size)
@@ -74,7 +93,7 @@ class DataSet(torch.utils.data.Dataset):
             else:
                 self.output_data_ph = torch.from_numpy(self.train_data_ph[2]).view(self.train_data_ph[2].shape[0]*self.train_data_ph[2].shape[1])
             #--------------------------------------------------------------
-        elif sample_mode == 2:
+        elif PINN_sample_mode == 'split traj':
             self.data_length = int(self.data_length / 2)
             self.x_data = x[::2]
             self.z_data = z[::2]
@@ -83,7 +102,7 @@ class DataSet(torch.utils.data.Dataset):
             self.z_data_ph = z[1::2]
             self.output_data_ph = y[1::2]
         else:
-            raise Exception('Sample mode must be either 1 or 2.')
+            raise Exception('Sample mode must be either ''split set'' or ''split traj''.')
 
         # ----------------------- Mean and standard deviation -----------------------     
         self.mean_x = torch.mean(self.x_data, dim = 0)
@@ -101,31 +120,48 @@ class DataSet(torch.utils.data.Dataset):
         self.std_output_ph = torch.std(self.output_data, dim = 0)
         #-----------------------------------------------------------------------------
         self.time = torch.from_numpy(self.train_data[3])
-        self.M = M
-        self.K = K
-        self.system = system
         
-    def __len__(self):
+    def __len__(self) -> None:
         return self.data_length
     
-    def generate_data(self,system, M, K, a, b, N, samples, limits, seed):
-        ic = system.sample_ic(limits, samples, seed = seed)
-        x_data, output, t = system.generate_data(ic, a, b, N)
-        z_data = data.KKL_observer_data(M, K, output, a, b, N)
-        
-        return x_data, z_data, output, t, ic
+    def generate_data(self, seed: int) -> tuple:
+        """
+        System initial conditions are sampled with LHS. Data is generated by simulating
+        x and z systems. 
+        """
 
-    def set_physics_limits(self, limits):
+        t_back = -5
+        h = (self.b-self.a)/self.N
+        ic = self.system.sample_ic(self.limits_normal, self.samples, seed = seed)     # Sample initial conditions for x
+        x_data_fw, output_fw, t_fw = self.system.generate_data(ic, self.a, self.b, self.N)      # Forward simulation 
+
+        if self.data_gen_mode == 'backward sim':
+            _ , output_bw, t_bw = self.system.generate_data(ic, self.a, t_back, int(np.abs(t_back/h)))      # Backward simulation
+            output_bw = np.flip(output_bw, axis = 1)
+        elif self.data_gen_mode == 'negative forward':
+            _ , output_bw, t_bw = self.system.generate_data(ic, t_back, self.a, int(np.abs(t_back/h)))      # Backward simulation
+        else:
+            raise Exception('Data generation mode must be either ''backward sim'' or ''negative forward''.')
+
+        ic_z_bw = np.random.rand(self.samples, self.system.z_size)
+        z_data_fw1 = data.KKL_observer_data(self.M, self.K, output_bw, t_back, self.a, ic_z_bw, int(np.abs(t_back/h)))    
+        ic_z = z_data_fw1[:, -1, :]     # The last element of each trajectory
+        #ic_z = np.zeros([samples, self.system.z_size])
+        z_data_fw2 = data.KKL_observer_data(self.M, self.K, output_fw, self.a, self.b, ic_z, self.N)
+
+        return x_data_fw, z_data_fw2, output_fw, t_fw, ic
+
+    def set_physics_limits(self, limits: np.ndarray) -> None:
         """
         Sets the limits of the state sample space for the physics datapoints.
         Can only be used if sample_mode is 2.
         """
-        if self.sample_mode == 2:
+        if self.PINN_sample_mode == 'split traj':
             self.limit_physics = limits
         else:
             raise Exception('Can only set limits if sample mode is 1.')  
     
-    def normalize(self):
+    def normalize(self) -> None:
         """
         Old method to normalize all the data before training.
         Use the normalizer class instead.
@@ -134,11 +170,10 @@ class DataSet(torch.utils.data.Dataset):
         self.z_data = (self.z_data - self.mean_z) / self.std_z
         self.output_data = (self.output_data - self.mean_output) / self.std_output       
         
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> None:
         x = self.x_data[idx]
         z = self.z_data[idx]
         y = self.output_data[idx]
         x_ph = self.x_data_ph[idx]
         y_ph = self.output_data_ph[idx]
         return [x.float(), z.float(), y.float(), x_ph.float(), y_ph.float()]
-      
